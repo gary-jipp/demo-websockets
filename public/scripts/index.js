83e801ea292d7c6084b3d9c4961dae5ce564e13c
Gary J <github@accu.ca>
2022-03-16 16:40:25 -0700

    Update

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -11,7 +11,7 @@ $(function() {
     if (!text) {
       return;
     }
-    
+
     const to = $("#to").val();
     console.log("to=", to);
 
@@ -49,12 +49,12 @@ $(function() {
 
 });
 
+// Generates a random email address
 const randomEmail = function(size) {
   const chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
   let string = chars[Math.floor(Math.random() * 26)];
   for (let i = 0; i < size - 1; i++) {
     string += chars[Math.floor(Math.random() * chars.length)];
   }
-
   return (string + '@gmail.com');
 };
